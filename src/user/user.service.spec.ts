b3a5799034aafec83286b9f206a7848d25f55007
Alex Xiang <alex.xiang@dltx.io>
2022-08-10 21:42:49 +1000

    fix user service and controller unit test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { UserModule } from "./user.module";
+import { Test, TestingModule } from "@nestjs/testing";
+import { UserService } from "./user.service";
+import { expect } from "chai";
+import { DatabaseModule } from "../data/database.module";
+import { userProviders } from "./user.providers";
+import { ConfigModule } from "@nestjs/config";
+import { IUserService } from "src/interfaces";
 
-describe('UserService', () => {
-  let service: UserService;
+describe("UserService", () => {
+    let service: IUserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                DatabaseModule,
+                UserModule,
+                ConfigModule.forRoot({
+                    isGlobal: true
+                })
+            ],
+            providers: [...userProviders, UserService]
+        }).compile();
 
-    service = module.get<UserService>(UserService);
-  });
+        service = module.get<IUserService>(UserService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(service).to.be.not.undefined;
+    });
+
+    it("should return an array of users", async () => {
+        const users = await service.findAll();
+        expect(users).to.be.an("array");
+    });
 });
