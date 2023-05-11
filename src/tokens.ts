07b767b127e283c305148f0b2f424c48dd66e27a
Barry Earsman <barry@earsman.com>
2023-05-11 12:06:44 +1000

    chore: remove mongo

--- src/tokens.ts
+++ src/tokens.ts
@@ -2,7 +2,5 @@ export enum ProviderTokens {
     UserService = "UserService",
     ExampleService = "ExampleService",
     PostgresDataSource = "PostgresDataSource",
-    MongoDbDataSource = "MongoDbDataSource",
-    UserMongo = "UserMongo",
     UserPostgres = "UserPostgres"
 }
